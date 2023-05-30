c2ca98d9730f3c66be203f2da7677a8cb811bec9
Siddharth Saraf <sidsrf@gmail.com>
2023-05-30 10:51:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fcc-backend-filemetadata
+
+This is the [5th project](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice) under FreeCodeCamp Backend and API certification projects.
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
+require('dotenv').config();
+
+
+const app = express()
+const upload = multer({ dest: 'uploads/' })
+
+app.use(cors())
+app.use(express.static(__dirname + '/assets'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.post('/api/getfm', upload.single('upfile'), (req, res) => {
+    const f = req.file;
+    res.json({ "name": f.originalname, "type": f.mimetype, "size": f.size })
+})
+
+const port = process.env.port || 3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+// By Siddharth Saraf
